a196480cc3047fb2f91db32649ada28707db2367
AryanIssPro <aryan0106gupta@gmail.com>
2024-11-11 19:10:57 +0530

    11-11-2024 07:10:50

--- Python/autoclicker.py
+++ Python/autoclicker.py
@@ -1,12 +1,26 @@
-from pynput.mouse import Button, Controller
-from pynput.keyboard import Key, Listener
+from pynput.mouse import Button, Controller as MouseController
+from pynput.keyboard import Controller as KeyboardController
+from pynput.keyboard import Listener as KeyboardListener
 from time import sleep
 
-mouse = Controller()
+# Instantiate the keyboard and mouse controllers
+keyboard = KeyboardController()
+mouse = MouseController()
 
+# Define the function to perform repeated actions
 def click():
-    print("performed!")
-    mouse.click(Button.left, 1560)
+    print("Performed!")
+    mouse.click(Button.left, 100)  # Performs a left-click with the mouse
+    sleep(0.1)
 
-sleep(5)
-click()
+# Define what happens on each key press
+def on_press(key):
+    try:
+        if key.char == ';':
+            click()
+    except AttributeError:
+        pass
+
+# Start listening to keyboard events
+with KeyboardListener(on_press=on_press) as listener:
+    listener.join()
